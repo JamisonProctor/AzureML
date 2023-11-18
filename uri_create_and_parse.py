ca0ab11d56ee88882c393a2436dd5e1159ce05a9
Jamison Proctor <jamison.proctor@gmail.com>
2023-11-18 15:05:40 +0100

    add mltable format and boilerplate code for creating and parsing Azure Data Assets

---
+++ uri_create_and_parse.py
@@ -0,0 +1,85 @@
+# create a URI file data asset.
+from azure.ai.ml.entities import Data
+from azure.ai.ml.constants import AssetTypes
+
+my_path = '<supported-path>'
+
+my_data = Data(
+    path=my_path,
+    type=AssetTypes.URI_FILE,
+    description="<description>",
+    name="<name>",
+    version="<version>"
+)
+
+ml_client.data.create_or_update(my_data)
+
+# parse a data asset from a URI file. 
+import argparse
+import pandas as pd
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--input_data", type=str)
+args = parser.parse_args()
+
+df = pd.read_csv(args.input_data)
+print(df.head(10))
+
+# Create a URI folder data asset.
+from azure.ai.ml.entities import Data
+from azure.ai.ml.constants import AssetTypes
+
+my_path = '<supported-path>'
+
+my_data = Data(
+    path=my_path,
+    type=AssetTypes.URI_FOLDER,
+    description="<description>",
+    name="<name>",
+    version='<version>'
+)
+
+ml_client.data.create_or_update(my_data)
+
+# Parse a data asset from a URI folder.
+import argparse
+import glob
+import pandas as pd
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--input_data", type=str)
+args = parser.parse_args()
+
+data_path = args.input_data
+all_files = glob.glob(data_path + "/*.csv")
+df = pd.concat((pd.read_csv(f) for f in all_files), sort=False)
+
+# Create an MLTable data asset.
+from azure.ai.ml.entities import Data
+from azure.ai.ml.constants import AssetTypes
+
+my_path = '<path-including-mltable-file>'
+
+my_data = Data(
+    path=my_path,
+    type=AssetTypes.MLTABLE,
+    description="<description>",
+    name="<name>",
+    version='<version>'
+)
+
+ml_client.data.create_or_update(my_data)
+
+# Parse an MLTable data asset.
+import argparse
+import mltable
+import pandas
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--input_data", type=str)
+args = parser.parse_args()
+
+tbl = mltable.load(args.input_data)
+df = tbl.to_pandas_dataframe()
+
+print(df.head(10))
